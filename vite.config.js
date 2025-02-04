77dafdc69a8ea95eb845c400a8142bcb15038aca
mhmd mhdi <mhdimhdin5@gmail.com>
2025-02-04 12:09:20 +0330

    add deploy file and secret acccess and change vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'Job-Seeker-Initial-Registration-Page-',
+  build:{
+    outDir:'dist'
+  }
 })
