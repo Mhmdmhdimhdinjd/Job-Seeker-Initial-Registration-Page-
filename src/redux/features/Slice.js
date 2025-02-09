f9345aedbc5a4c81b3ddd694e4cd833f3f1a908a
mhmd mhdi <mhdimhdin5@gmail.com>
2025-02-09 09:40:54 +0330

    add initial state

--- src/redux/features/Slice.js
+++ src/redux/features/Slice.js
@@ -3,10 +3,47 @@ import { createSlice } from '@reduxjs/toolkit';
 const counterSlice = createSlice({
   name: 'job_seeker',
   initialState: {
-    PersonalInformation: 0,
+    PersonalInformation: [
+      {
+        "firstName": "محمد مهدی",
+        "lastName": "مهدی نژاد",
+        "phoneNumber": "09114261502",
+        "nationalCode": "2051398240",
+        "gender": "male",
+        "province": {
+          "value": "mazandaran",
+          "label": "مازندران"
+        },
+        "city": {
+          "value": "sari",
+          "label": "ساری"
+        },
+        "categories": [
+          {
+            "value": "health",
+            "label": "سلامت"
+          }
+        ],
+        "interests": [
+          {
+            "value": "reading1",
+            "label": "1کتابخوانی"
+          },
+          {
+            "value": "music",
+            "label": "موسیقی"
+          },
+          {
+            "value": "reading3",
+            "label": "3کتابخوانی"
+          }
+        ],
+        "ck": "<p>عتغافلقربزیسطیزبرلفقفاغعتزبرلاذتنمکجمحنخاعتغلب</p>"
+      }
+    ],
   },
   reducers: {
-    setInformation: (state , action ) => {
+    setInformation: (state, action) => {
       state.PersonalInformation = action.payload;
       console.log(state.PersonalInformation)
     },
