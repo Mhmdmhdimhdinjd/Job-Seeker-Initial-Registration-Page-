aa542bd3ce9e7fb50b0aa9fa237de16df34d2b22
mhmd mhdi <mhdimhdin5@gmail.com>
2025-02-11 09:41:06 +0330

    fix phone number yup

--- src/Pages/HomeContainer.jsx
+++ src/Pages/HomeContainer.jsx
@@ -47,7 +47,7 @@ const schema = yup.object().shape({
     firstName: yup.string().matches(/^[\u0600-\u06FF\s]+$/, 'فقط حروف فارسی مجاز است').required('نام ضروری است'),
     lastName: yup.string().matches(/^[\u0600-\u06FF\s]+$/, 'فقط حروف فارسی مجاز است').required('نام خانوادگی ضروری است'),
     nationalCode: yup.string().matches(/^\d{10}$/, 'کد ملی باید 10 رقم باشد').required('کد ملی ضروری است'),
-    phoneNumber: yup.string().matches(/^09\d{11}$/, 'شماره تماس باید 11 رقم و با 09 شروع شود').required('شماره تماس ضروری است'),
+    phoneNumber: yup.string().matches(/^09\d{9}$/, 'شماره تماس باید 11 رقم و با 09 شروع شود').required('شماره تماس ضروری است'),
     geneder:yup.string().required('لطفا جنسیت را وارد کنید'),
     categories: yup.array().max(2, 'حداکثر ۲ دسته‌بندی می‌توانید انتخاب کنید').required('انتخاب دسته‌بندی‌ها ضروری است'),
     interests: yup.array().max(3, 'حداکثر ۳ گزینه می‌توانید انتخاب کنید').required('انتخاب گزینه‌ها اجباری است'),
